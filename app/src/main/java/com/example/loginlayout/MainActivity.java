35d1e35a909078205f1186ac9dbd1649f11c0510
Ramadani Kurnia Kito <ramadanikurniakito@gmail.com>
2024-04-15 09:23:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loginlayout/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.loginlayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        };
+        public void pindah(View view) {
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
